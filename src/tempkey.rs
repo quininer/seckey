11140463b9b3ca8d0bd007c8a356a67d7ff1679c
quininer <quininer@live.com>
2018-01-10 22:56:27 +0800

    feat: TempKey support slice

--- src/tempkey.rs
+++ src/tempkey.rs
@@ -1,7 +1,8 @@
 use core::{ fmt, mem };
-use core::cmp::Ordering;
+use core::cmp::{ self, Ordering };
 use core::ops::{ Deref, DerefMut };
-use memsec::{ memeq, memcmp, memzero };
+use memsec::{ memeq, memcmp };
+#[cfg(not(feature = "use_std"))] use memsec::memzero;
 #[cfg(feature = "use_std")] use memsec::{ mlock, munlock };
 
 
@@ -14,39 +15,63 @@ use memsec::{ memeq, memcmp, memzero };
 /// let key = TempKey::new(&mut key);
 /// assert_eq!(key, [8u8; 8]);
 /// assert_ne!(key, [1u8; 8]);
+///
 /// let mut key2 = [8u8; 8];
-/// assert_eq!(key, TempKey::new(&mut key2));
+/// assert_eq!(key, *TempKey::new(&mut key2));
 /// ```
-pub struct TempKey<'a, T: Sized + 'a>(&'a mut T);
+pub struct TempKey<'a, T: ?Sized + 'a>(&'a mut T);
 
 pub struct NeedsDrop;
 
+
+impl<'a, T: ?Sized + Copy> TempKey<'a, T> {
+    pub fn new(t: &'a mut T) -> TempKey<'a, T> {
+        #[cfg(feature = "use_std")]
+        unsafe { mlock(t as *mut T as *mut u8, mem::size_of_val(t)) };
+
+        TempKey(t)
+    }
+}
+
 // TODO use TryFrom
 // :( https://github.com/rust-lang/rust/issues/33417#issuecomment-347046063
-impl<'a, T> TempKey<'a, T> where T: Sized + 'a {
+impl<'a, T: Sized> TempKey<'a, T> {
     pub fn try_from(t: &'a mut T) -> Result<TempKey<'a, T>, NeedsDrop> {
         if mem::needs_drop::<T>() {
             Err(NeedsDrop)
         } else {
             #[cfg(feature = "use_std")]
-            unsafe { mlock(t, mem::size_of::<T>()) };
+            unsafe { mlock(t, mem::size_of_val(t)) };
 
             Ok(TempKey(t))
         }
     }
 }
 
-impl<'a, T> TempKey<'a, T> where T: Sized + Copy + 'a {
-    pub fn new(t: &'a mut T) -> TempKey<'a, T> {
+impl<'a, T: Sized + Copy> TempKey<'a, [T]> {
+    pub fn from_slice(t: &'a mut [T]) -> TempKey<'a, [T]> {
         #[cfg(feature = "use_std")]
-        unsafe { mlock(t, mem::size_of::<T>()) };
+        unsafe { mlock(t.as_mut_ptr() as *mut u8, mem::size_of_val(t)) };
 
         TempKey(t)
     }
 }
 
+impl<'a, T: Sized> TempKey<'a, [T]> {
+    pub fn try_from_slice(t: &'a mut [T]) -> Result<TempKey<'a, [T]>, NeedsDrop> {
+        if mem::needs_drop::<T>() {
+            Err(NeedsDrop)
+        } else {
+            #[cfg(feature = "use_std")]
+            unsafe { mlock(t.as_mut_ptr() as *mut u8, mem::size_of_val(t)) };
 
-impl<'a, T> Deref for TempKey<'a, T> where T: Sized + 'a {
+            Ok(TempKey(t))
+        }
+    }
+}
+
+
+impl<'a, T: ?Sized> Deref for TempKey<'a, T> {
     type Target = T;
 
     fn deref(&self) -> &T {
@@ -55,13 +80,13 @@ impl<'a, T> Deref for TempKey<'a, T> where T: Sized + 'a {
 }
 
 
-impl<'a, T> DerefMut for TempKey<'a, T> where T: Sized + 'a {
+impl<'a, T: ?Sized> DerefMut for TempKey<'a, T> {
     fn deref_mut(&mut self) -> &mut T {
         self.0
     }
 }
 
-impl<'a, T> fmt::Debug for TempKey<'a, T> where T: Sized + 'a {
+impl<'a, T: ?Sized> fmt::Debug for TempKey<'a, T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         f.debug_tuple("TempKey")
             .field(&format_args!("{:p}", self.0))
@@ -69,57 +94,75 @@ impl<'a, T> fmt::Debug for TempKey<'a, T> where T: Sized + 'a {
     }
 }
 
-impl<'a, T> PartialEq<T> for TempKey<'a, T> where T: Sized + 'a {
+impl<'a, T: ?Sized> PartialEq<T> for TempKey<'a, T> {
     /// Constant time eq.
     ///
     /// NOTE, it compare memory value.
     fn eq(&self, rhs: &T) -> bool {
-        unsafe { memeq(self.0, rhs, mem::size_of::<T>()) }
+        let len1 = mem::size_of_val(self.0);
+        let len2 = mem::size_of_val(rhs);
+
+        let r = unsafe { memeq(
+            self.0 as *const T as *const u8,
+            rhs as *const T as *const u8,
+            len1
+        ) };
+        len1 == len2 && r
     }
 }
 
-impl<'a, 'b, T> PartialEq<TempKey<'b, T>> for TempKey<'a, T> where T: Sized + 'a {
+impl<'a, 'b, T: Sized> PartialEq<TempKey<'b, T>> for TempKey<'a, T> {
     /// Constant time eq.
     ///
     /// NOTE, it compare memory value.
-    #[inline]
     fn eq(&self, rhs: &TempKey<T>) -> bool {
-        self.eq(rhs as &T)
+        self.eq(rhs.deref())
     }
 }
 
-impl<'a, T> Eq for TempKey<'a, T> where T: Sized + 'a {}
+impl<'a, T: Sized> Eq for TempKey<'a, T> {}
 
-impl<'a, T> PartialOrd<T> for TempKey<'a, T> where T: Sized + 'a {
+impl<'a, T: ?Sized> PartialOrd<T> for TempKey<'a, T> {
     /// Constant time cmp.
     ///
     /// NOTE, it compare memory value.
     fn partial_cmp(&self, rhs: &T) -> Option<Ordering> {
-        let order =
-            unsafe { memcmp(self.0, rhs, mem::size_of::<T>()) };
-        Some(order.cmp(&0))
+        let len1 = mem::size_of_val(self.0);
+        let len2 = mem::size_of_val(rhs);
+
+        let order = unsafe { memcmp(
+            self.0 as *const T as *const u8,
+            rhs as *const T as *const u8,
+            cmp::min(len1, len2))
+        };
+
+        Some(match order.cmp(&0) {
+            Ordering::Equal => len1.cmp(&len2),
+            order => order
+        })
     }
 }
 
-impl<'a, 'b, T> PartialOrd<TempKey<'b, T>> for TempKey<'a, T> where T: Sized + 'a {
-    #[inline]
+impl<'a, 'b, T: Sized> PartialOrd<TempKey<'b, T>> for TempKey<'a, T> {
     fn partial_cmp(&self, rhs: &TempKey<T>) -> Option<Ordering> {
-        self.partial_cmp(rhs as &T)
+        self.partial_cmp(rhs.deref())
     }
 }
 
-impl<'a, T> Ord for TempKey<'a, T> where T: Sized + 'a {
-    #[inline]
+impl<'a, T: Sized> Ord for TempKey<'a, T> {
     fn cmp(&self, rhs: &TempKey<T>) -> Ordering {
         self.partial_cmp(rhs).unwrap()
     }
 }
 
-impl<'a, T> Drop for TempKey<'a, T> where T: Sized {
+impl<'a, T: ?Sized> Drop for TempKey<'a, T> {
     fn drop(&mut self) {
-        #[cfg(feature = "use_std")]
-        unsafe { munlock(self.0, mem::size_of::<T>()) };
+        let size = mem::size_of_val(self.0);
 
-        unsafe { memzero(self.0, mem::size_of::<T>()) };
+        #[cfg(feature = "use_std")]
+        unsafe { munlock(self.0 as *mut T as *mut u8, size) };
+
+        #[cfg(not(feature = "use_std"))]
+        unsafe { memzero(self.0 as *mut T as *mut u8, size) };
     }
 }
--- tests/tempkey.rs
+++ tests/tempkey.rs
@@ -8,6 +8,8 @@ use seckey::TempKey;
 #[test]
 fn tempkey_cmp_test() {
     let mut one: i32 = 1;
+    let mut one2: i32 = 1;
+    let mut one3: i32 = 1;
     let mut zero: i32 = 0;
     let mut negative_one: i32 = -1;
 
@@ -17,11 +19,14 @@ fn tempkey_cmp_test() {
 
     assert!(TempKey::new(&mut negative_one) > 0);
         // ^- NOTE 4294967295 > 0
+
+    assert_eq!(TempKey::new(&mut one2), TempKey::new(&mut one3));
 }
 
 #[test]
 fn tempkey_slice_test() {
-    let mut key = [42u8; 8];
+    // fixed size
+    let mut key = [42u32; 8];
 
     {
         let mut tempkey = TempKey::new(&mut key);
@@ -32,20 +37,42 @@ fn tempkey_slice_test() {
     }
 
     assert_eq!(key, [0; 8]);
+
+
+    // dyn size
+    let mut key = [42u32; 8];
+
+    {
+        let mut tempkey = TempKey::from_slice(&mut key[1..7]);
+        assert_eq!(tempkey, [42; 6][..]);
+
+        tempkey[1] = 0;
+        assert_eq!(tempkey, [42, 0, 42, 42, 42, 42][..]);
+    }
+
+    assert_eq!(&key[1..7], [0; 6]);
+    assert_eq!(key[0], 42);
+    assert_eq!(key[7], 42);
 }
 
 #[test]
 fn tempkey_try_form_test() {
-    struct Bar(());
-    struct Bar2(());
+    struct Bar<T>(T);
+    struct Bar2<T>(T);
 
-    impl Drop for Bar2 {
+    impl<T> Drop for Bar2<T> {
         fn drop(&mut self) {}
     }
 
     let mut bar = Bar(());
     let mut bar2 = Bar2(());
+    let mut bar3 = Bar(Bar2(()));
+    let mut bar_slice = [Bar(()), Bar(()), Bar(())];
+    let mut bar_slice2 = [Bar2(()), Bar2(()), Bar2(())];
 
     assert!(TempKey::try_from(&mut bar).is_ok());
     assert!(TempKey::try_from(&mut bar2).is_err());
+    assert!(TempKey::try_from(&mut bar3).is_err());
+    assert!(TempKey::try_from_slice(&mut bar_slice[..]).is_ok());
+    assert!(TempKey::try_from_slice(&mut bar_slice2[..]).is_err());
 }
